a38aacad1636490f679dce688748e50a52f2e8ac
ghizooghizoo <benmoumenghizlane55@gmail.com>
2024-01-26 14:02:04 -0800

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,8 @@
 
 exec {'replace':
 provider => shell,
-command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+command => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/
+" /etc/default/nginx',
 before => Exec['restart'],
 }
 
